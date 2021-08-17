f86cc1dea0f8b8e519b54e381c967b4e26354ab2
Moritz <moritzsontheimer@gmail.com>
2021-08-17 14:02:41 +0800

    learning log working

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
 
     # Detail page for a single topic
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # Pagoe for adding a new entry
+    re_path(r'new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
